f0400d0e89256375f48a0c093d6d13987a53bccc
Jan-Ingar Tistel <jan-ingar.tistel@digdir.no>
2025-09-25 07:52:05 +0200

    EUW-568 endre url for issuer (#19)
    # Conflicts:
    #	core-logic/src/demo/java/eu/europa/ec/corelogic/config/WalletCoreConfigImpl.kt
    #	core-logic/src/dev/java/eu/europa/ec/corelogic/config/WalletCoreConfigImpl.kt

--- core-logic/src/demo/java/eu/europa/ec/corelogic/config/WalletCoreConfigImpl.kt
+++ core-logic/src/demo/java/eu/europa/ec/corelogic/config/WalletCoreConfigImpl.kt
@@ -46,6 +46,12 @@ internal class WalletCoreConfigImpl : WalletCoreConfig {
         const val AUTHENTICATION_REQUIRED = false
     }
 
+    private companion object {
+        const val VCI_ISSUER_URL = "https://utsteder.test.eidas2sandkasse.net"
+        const val VCI_CLIENT_ID = "wallet-dev"
+        const val AUTHENTICATION_REQUIRED = false
+    }
+
     private var _config: EudiWalletConfig? = null
 
     override val config: EudiWalletConfig
